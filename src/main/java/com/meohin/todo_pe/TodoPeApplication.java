4f1ba0afda7d3eb50a25125caf55868022b551d9
meohin <meohin.meo@gmail.com>
2023-05-05 16:41:24 +0900

    Add default settings

---
+++ src/main/java/com/meohin/todo_pe/TodoPeApplication.java
@@ -0,0 +1,13 @@
+package com.meohin.todo_pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoPeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoPeApplication.class, args);
+    }
+
+}
