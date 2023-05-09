c0835cb8053489be92d1e6f98d2448a2f4aeb08b
meohin <meohin.meo@gmail.com>
2023-05-09 17:16:28 +0900

    Use lombok annotations

--- src/main/java/com/meohin/todo_pe/User.java
+++ src/main/java/com/meohin/todo_pe/User.java
@@ -1,10 +1,11 @@
 package com.meohin.todo_pe;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity // Hibernate에게 이 클래스에서 테이블을 만들도록 지시
 public class User {
 
@@ -12,31 +13,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 
+    @Column(unique = true)
     private String email;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
