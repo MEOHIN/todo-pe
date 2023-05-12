df1ccc57ee93637b44bdbda2b7a3a38c623e8aca
meohin <meohin.meo@gmail.com>
2023-05-12 17:38:24 +0900

    Update user entity

--- src/main/java/com/meohin/todo_pe/user/User.java
+++ src/main/java/com/meohin/todo_pe/user/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Entity // Hibernate에게 이 클래스에서 테이블을 만들도록 지시
@@ -14,8 +16,10 @@ public class User {
     private Long id;
 
     @Column(unique = true)
-    private String name;
+    private String snsType;
 
     @Column(unique = true)
-    private String email;
+    private String snsID;
+
+    private LocalDateTime authorizeDate;
 }
