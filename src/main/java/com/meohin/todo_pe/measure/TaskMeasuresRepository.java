6d5ccb701ccfb16a9f4dd9d6f4f700b3b09531e4
meohin <meohin.meo@gmail.com>
2023-05-18 14:10:35 +0900

    Add JpaRepository interface

---
+++ src/main/java/com/meohin/todo_pe/measure/TaskMeasuresRepository.java
@@ -0,0 +1,6 @@
+package com.meohin.todo_pe.measure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskMeasuresRepository extends JpaRepository<TaskMeasures, Integer> {
+}
