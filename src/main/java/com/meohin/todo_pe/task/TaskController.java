3f01e1a538e4f0df58f44067c11086eb0c5090f9
meohin <meohin.meo@gmail.com>
2023-05-23 20:44:04 +0900

    Update to return task_list.html template

--- src/main/java/com/meohin/todo_pe/task/TaskController.java
+++ src/main/java/com/meohin/todo_pe/task/TaskController.java
@@ -2,14 +2,12 @@ package com.meohin.todo_pe.task;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TaskController {
 
     @RequestMapping("/task/list")
-    @ResponseBody
     public String list() {
-        return "task list";
+        return "task_list";
     }
 }
