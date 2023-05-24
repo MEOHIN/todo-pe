421b22044d5d43469d6fbb3b882ff5b2fea402fc
meohin <meohin.meo@gmail.com>
2023-05-24 17:02:55 +0900

    Add @RequiredArgsConstructor so that TaskRepository is injected when TaskController object is created

--- src/main/java/com/meohin/todo_pe/task/TaskController.java
+++ src/main/java/com/meohin/todo_pe/task/TaskController.java
@@ -1,8 +1,10 @@
 package com.meohin.todo_pe.task;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor    // TaskController 객체가 생성될 때 TaskRepository가 주입되도록 한다.
 @Controller
 public class TaskController {
 
