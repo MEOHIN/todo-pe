72e66a6c6c96e60f4be14c6595e0b0312d23092e
meohin <meohin.meo@gmail.com>
2023-05-24 17:08:51 +0900

    Update list() method to found all tasks and add the tasks to Model object

--- src/main/java/com/meohin/todo_pe/task/TaskController.java
+++ src/main/java/com/meohin/todo_pe/task/TaskController.java
@@ -2,7 +2,10 @@ package com.meohin.todo_pe.task;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor    // TaskController 객체가 생성될 때 TaskRepository가 주입되도록 한다.
 @Controller
@@ -11,7 +14,9 @@ public class TaskController {
     private final TaskRepository taskRepository;    // TaskRepository를 사용하여 작업 목록을 조회할 수 있도록 한다.
 
     @RequestMapping("/task/list")
-    public String list() {
+    public String list(Model model) {
+        List<Task> taskList = this.taskRepository.findAll();    // 모든 작업(Task)을 조회한다.
+        model.addAttribute("taskList", taskList);   // 조회된 작업 목록을 Model 객체에 추가한다.
         return "task_list";
     }
 }
