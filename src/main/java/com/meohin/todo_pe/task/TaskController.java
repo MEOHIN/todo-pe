e759e266bdab43974c234bfb730c2b44708c88c5
meohin <meohin.meo@gmail.com>
2023-05-26 16:04:44 +0900

    Update task controller to process data using task service

--- src/main/java/com/meohin/todo_pe/task/TaskController.java
+++ src/main/java/com/meohin/todo_pe/task/TaskController.java
@@ -11,12 +11,12 @@ import java.util.List;
 @Controller
 public class TaskController {
 
-    private final TaskRepository taskRepository;    // TaskRepository를 사용하여 작업 목록을 조회할 수 있도록 한다.
+    private final TaskService taskService;    // TaskRepository를 사용하여 작업 목록을 조회할 수 있도록 한다.
 
     @RequestMapping("/task/list")
     public String list(Model model) {
-        List<Task> taskList = this.taskRepository.findAll();    // 모든 작업(Task)을 조회한다.
-        model.addAttribute("taskList", taskList);   // 조회된 작업 목록을 Model 객체에 추가한다.
+        List<TaskDTO> taskDTOList = taskService.getTaskList();    // 모든 작업(Task)을 조회한다.
+        model.addAttribute("taskList", taskDTOList);   // 조회된 작업 목록을 Model 객체에 추가한다.
         return "task_list";
     }
 }
