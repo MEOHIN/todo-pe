1957db63f0076a00f18b5c141614d992620e52aa
meohin <meohin.meo@gmail.com>
2023-05-30 15:26:21 +0900

    Update to apply prefix to "/task"

--- src/main/java/com/meohin/todo_pe/task/TaskController.java
+++ src/main/java/com/meohin/todo_pe/task/TaskController.java
@@ -10,18 +10,19 @@ import java.util.List;
 
 @RequiredArgsConstructor    // TaskController 객체가 생성될 때 TaskRepository가 주입되도록 한다.
 @Controller
+@RequestMapping("/task")    // 프리픽스 지정
 public class TaskController {
 
     private final TaskService taskService;    // TaskRepository를 사용하여 작업 목록을 조회할 수 있도록 한다.
 
-    @RequestMapping("/task/list")
+    @RequestMapping("/list")
     public String list(Model model) {
         List<TaskDTO> taskDTOList = taskService.getTaskList();    // 모든 작업(Task)을 조회한다.
         model.addAttribute("taskList", taskDTOList);   // 조회된 작업 목록을 Model 객체에 추가한다.
         return "task_list";
     }
 
-    @RequestMapping(value = "/task/detail/{taskId}")
+    @RequestMapping(value = "/detail/{taskId}")
     public String detail(@PathVariable("taskId") Integer taskId, Model model) {
         TaskDTO taskDTO = taskService.getTaskById(taskId);
         model.addAttribute("task", taskDTO);
