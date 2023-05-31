4c41eb8d115dadbe798df2f812fd2a0085083984
meohin <meohin.meo@gmail.com>
2023-05-31 19:04:54 +0900

    Update docs about class and method

--- src/main/java/com/meohin/todo_pe/task/TaskController.java
+++ src/main/java/com/meohin/todo_pe/task/TaskController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+/**
+ * Task 컨틀롤러 클래스다.
+ */
 @RequiredArgsConstructor    // TaskController 객체가 생성될 때 TaskRepository가 주입되도록 한다.
 @Controller
 @RequestMapping("/task")    // 프리픽스 지정
@@ -15,13 +18,24 @@ public class TaskController {
 
     private final TaskService taskService;    // TaskRepository를 사용하여 작업 목록을 조회할 수 있도록 한다.
 
+    /**
+     * 모든 task를 조회하고, 조회된 목록을 Model 객체에 추가한다.
+     * @param model     Model 객체
+     * @return  Task 목록 템플릿
+     */
     @RequestMapping("/list")
     public String list(Model model) {
-        List<TaskDTO> taskDTOList = taskService.getTaskList();    // 모든 작업(Task)을 조회한다.
-        model.addAttribute("taskList", taskDTOList);   // 조회된 작업 목록을 Model 객체에 추가한다.
+        List<TaskDTO> taskDTOList = taskService.getTaskList();
+        model.addAttribute("taskList", taskDTOList);
         return "task_list";
     }
 
+    /**
+     * 작업 상세 정보를 조회한다.
+     * @param taskId    Task ID
+     * @param model     Model 객체
+     * @return  Task 상세 템플릿
+     */
     @RequestMapping(value = "/detail/{taskId}")
     public String detail(@PathVariable("taskId") Integer taskId, Model model) {
         TaskDTO taskDTO = taskService.getTaskById(taskId);
