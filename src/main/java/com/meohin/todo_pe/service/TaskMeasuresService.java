9ed55f3fe112e1c923ee6158dda9b92388cf94ed
meohin <meohin.meo@gmail.com>
2023-06-01 17:31:07 +0900

    Add method to save TaskMeasures via taskMeasuresRepository

--- src/main/java/com/meohin/todo_pe/service/TaskMeasuresService.java
+++ src/main/java/com/meohin/todo_pe/service/TaskMeasuresService.java
@@ -1,8 +1,17 @@
 package com.meohin.todo_pe.service;
 
+import com.meohin.todo_pe.entity.TaskMeasures;
+import com.meohin.todo_pe.repository.TaskMeasuresRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class TaskMeasuresService {
 
+    private final TaskMeasuresRepository taskMeasuresRepository;
+
+    public void addTaskMeasures(TaskMeasures taskMeasures) {
+        taskMeasuresRepository.save(taskMeasures);
+    }
 }
