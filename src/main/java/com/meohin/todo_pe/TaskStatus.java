87bb38e12d153ee77f09a8a8c559c3fd6ef23196
meohin <meohin.meo@gmail.com>
2023-06-13 19:50:46 +0900

    Implement method to find corresponding Enum through DB data

--- src/main/java/com/meohin/todo_pe/TaskStatus.java
+++ src/main/java/com/meohin/todo_pe/TaskStatus.java
@@ -2,6 +2,8 @@ package com.meohin.todo_pe;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 public enum TaskStatus {
     STANDBY("STANDBY"),
@@ -12,5 +14,12 @@ public enum TaskStatus {
 
     TaskStatus(String value) {
         this.value = value;
+    }
+
+    public static TaskStatus ofCode(String dbData) {
+        return Arrays.stream(TaskStatus.values())
+                .filter(v -> v.getValue().equals(dbData))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(String.format("Task 상태에 %s가 존재하지 않습니다.", dbData)));
     }
 }
