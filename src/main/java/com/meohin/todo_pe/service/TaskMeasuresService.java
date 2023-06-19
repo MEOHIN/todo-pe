a27d66a73457daa7ebc73631ed9f97561e89895f
meohin <meohin.meo@gmail.com>
2023-06-19 18:08:17 +0900

    Add business logic design to add task history

--- src/main/java/com/meohin/todo_pe/service/TaskMeasuresService.java
+++ src/main/java/com/meohin/todo_pe/service/TaskMeasuresService.java
@@ -6,4 +6,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class TaskMeasuresService {
+
+    // 리턴 타입: void
+    // 파라미터: task 객체
+    // TaskMeasures 객체를 생성
+    // task 이력 설정
+    // task 이력 저장
 }
