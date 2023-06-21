2a890549827235dbe18e50f6309e9a21b9e1df0c
meohin <meohin.meo@gmail.com>
2023-06-21 13:38:20 +0900

    Add method to find data by Task ID and completeTime

--- src/main/java/com/meohin/todo_pe/repository/TaskMeasuresRepository.java
+++ src/main/java/com/meohin/todo_pe/repository/TaskMeasuresRepository.java
@@ -3,5 +3,9 @@ package com.meohin.todo_pe.repository;
 import com.meohin.todo_pe.entity.TaskMeasures;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface TaskMeasuresRepository extends JpaRepository<TaskMeasures, Long> {
+
+    TaskMeasures findByTaskIdAndCompleteTimeNull(Long taskId, LocalDateTime completeTime);
 }
