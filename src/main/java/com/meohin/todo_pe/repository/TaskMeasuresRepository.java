6f97e9afcf2127376ee840885b3f538b8920809e
meohin <meohin.meo@gmail.com>
2023-06-28 17:26:50 +0900

    Add method to find data by Task ID

--- src/main/java/com/meohin/todo_pe/repository/TaskMeasuresRepository.java
+++ src/main/java/com/meohin/todo_pe/repository/TaskMeasuresRepository.java
@@ -3,7 +3,11 @@ package com.meohin.todo_pe.repository;
 import com.meohin.todo_pe.entity.TaskMeasures;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskMeasuresRepository extends JpaRepository<TaskMeasures, Long> {
 
     TaskMeasures findByTaskIdAndCompleteTimeNull(Long taskId);
+
+    List<TaskMeasures> findByTaskId(Long taskId);
 }
