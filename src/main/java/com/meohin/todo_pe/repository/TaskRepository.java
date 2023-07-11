3e9e20d2f91abb832468403303edfc8047b7fcbe
meohin <meohin.meo@gmail.com>
2023-07-11 15:04:28 +0900

    Add method to search for task that includes the keyword entered in subject

--- src/main/java/com/meohin/todo_pe/repository/TaskRepository.java
+++ src/main/java/com/meohin/todo_pe/repository/TaskRepository.java
@@ -3,5 +3,8 @@ package com.meohin.todo_pe.repository;
 import com.meohin.todo_pe.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findTasksBySubjectLike(String subject);
 }
