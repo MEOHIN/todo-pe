a34ec7e63176ec336b4992605b866f3f987751f9
meohin <meohin.meo@gmail.com>
2023-07-24 14:25:14 +0900

    Add setting to allow all unauthenticated request

--- src/main/java/com/meohin/todo_pe/SecurityConfig.java
+++ src/main/java/com/meohin/todo_pe/SecurityConfig.java
@@ -5,12 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(
+                        (authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers(
+                                new AntPathRequestMatcher("/**")).permitAll());
         return httpSecurity.build();
     }
 }
