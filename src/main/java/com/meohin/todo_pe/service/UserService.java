3e99fdae4509e28c901b0b5f065d9ae996b81586
meohin <meohin.meo@gmail.com>
2023-07-25 16:33:11 +0900

    Use BCryptPasswordEncoder for security of password

--- src/main/java/com/meohin/todo_pe/service/UserService.java
+++ src/main/java/com/meohin/todo_pe/service/UserService.java
@@ -3,6 +3,7 @@ package com.meohin.todo_pe.service;
 import com.meohin.todo_pe.entity.User;
 import com.meohin.todo_pe.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -11,11 +12,13 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public void saveUserInfo(String userId, String pw, String email) {
+    public User saveUserInfo(String userId, String pw, String email) {
         User user = new User();
         user.setUserId(userId);
-        user.setPassword(pw);
         user.setEmail(email);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(pw);
         userRepository.save(user);
+        return user;
     }
 }
