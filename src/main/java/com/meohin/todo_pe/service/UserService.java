02512894505331a17f41ebeedd8c596ce0f21a37
meohin <meohin.meo@gmail.com>
2023-07-25 16:37:30 +0900

    Use PasswordEncoder bean instead of creating a BCryptPasswordEncoder object

--- src/main/java/com/meohin/todo_pe/service/UserService.java
+++ src/main/java/com/meohin/todo_pe/service/UserService.java
@@ -3,7 +3,7 @@ package com.meohin.todo_pe.service;
 import com.meohin.todo_pe.entity.User;
 import com.meohin.todo_pe.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -11,13 +11,13 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User saveUserInfo(String userId, String pw, String email) {
         User user = new User();
         user.setUserId(userId);
+        user.setPassword(passwordEncoder.encode(pw));
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        user.setPassword(pw);
         userRepository.save(user);
         return user;
     }
