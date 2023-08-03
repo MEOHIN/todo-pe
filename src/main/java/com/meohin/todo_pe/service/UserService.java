e2be790e287d94425a77d0fc4f843a88eb1e352c
meohin <meohin.meo@gmail.com>
2023-08-03 13:44:51 +0900

    Add method to retrieve user ID

--- src/main/java/com/meohin/todo_pe/service/UserService.java
+++ src/main/java/com/meohin/todo_pe/service/UserService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -20,5 +22,15 @@ public class UserService {
         siteUser.setEmail(email);
         userRepository.save(siteUser);
         return siteUser;
+    }
+
+    public SiteUser getUser(String username) {
+        Optional<SiteUser> user = this.userRepository.findByUserId(username);
+
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new RuntimeException("User ID not found");
+        }
     }
 }
