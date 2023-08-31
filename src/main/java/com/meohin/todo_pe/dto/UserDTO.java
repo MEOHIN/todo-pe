f3597aeecdd38759c0e8f111cc84c02470dfd5a6
meohin <meohin.meo@gmail.com>
2023-08-31 19:16:20 +0900

    Add @Pattern annotation to specify a regex pattern to validate the ID and password

--- src/main/java/com/meohin/todo_pe/dto/UserDTO.java
+++ src/main/java/com/meohin/todo_pe/dto/UserDTO.java
@@ -2,6 +2,7 @@ package com.meohin.todo_pe.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,9 +10,11 @@ import lombok.Setter;
 @Setter
 public class UserDTO {
 
+    @Pattern(regexp = "^[a-z0-9_-]{5,20}$", message = "아이디는 5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.")
     @NotEmpty(message = "ID는 필수 입력 항목입니다.")
     private String userId;
 
+    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_-]{8,16}$", message = "비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 사용해주세요.")
     @NotEmpty(message = "비밀번호는 필수 입력 항목입니다.")
     private String pw1;
 
