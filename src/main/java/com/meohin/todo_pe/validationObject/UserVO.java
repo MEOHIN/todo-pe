9ac18fcc934cc16817c7e981f48269cbe3cc0ab4
meohin <meohin.meo@gmail.com>
2023-09-26 19:33:14 +0900

    refactor: UserVO 필드 이름을 User 엔티티와 구분되게 수정

--- src/main/java/com/meohin/todo_pe/validationObject/UserVO.java
+++ src/main/java/com/meohin/todo_pe/validationObject/UserVO.java
@@ -15,17 +15,17 @@ public class UserVO {
 
     @Pattern(regexp = "^[a-z0-9_-]{5,20}$", message = "아이디는 5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.")
     @NotEmpty(message = "ID는 필수 입력 항목입니다.")
-    private String userId;
+    private String inputUserId;
 
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[\\W_]).{8,16}$", message = "비밀번호는 영문 대/소문자, 숫자, 특수문자를 각 하나 이상 포함하여 8~16자를 입력합니다.")
     @NotEmpty(message = "비밀번호는 필수 입력 항목입니다.")
-    private String pw1;
+    private String inputPW;
 
     @NotEmpty(message = "비밀번호 확인은 필수 입력 항목입니다.")
-    private String pw2;
+    private String confirmPW;
 
     @NotEmpty(message = "이메일은 필수 입력 항목입니다.")
     @Email
-    private String email;
+    private String inputEmail;
 
 }
