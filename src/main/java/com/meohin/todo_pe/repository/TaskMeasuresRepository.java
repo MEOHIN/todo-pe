bac3e1c5fecdee817d62bf141829aa141f5b8678
meohin <meohin.meo@gmail.com>
2023-12-01 17:52:47 +0900

    Refactor: TaskMeasuresRepository 메소드 주석 추가
    - findByTaskIdAndCompleteTimeNull 메소드의 주석은 완료 시간이 null인 TaskMeasures 엔티티를 찾는 기능을 설명합니다.
    - findByTaskIdAndCompleteTimeNotNull 메소드의 주석은 완료 시간이 null이 아닌 TaskMeasures 엔티티 목록을 찾는 기능을 설명합니다.
    - findByTaskId 메소드의 주석은 특정 taskId를 가진 TaskMeasures 엔티티 목록을 찾는 기능을 설명합니다.

--- src/main/java/com/meohin/todo_pe/repository/TaskMeasuresRepository.java
+++ src/main/java/com/meohin/todo_pe/repository/TaskMeasuresRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TaskMeasuresRepository extends JpaRepository<TaskMeasures, Long> {
-
+    // // 주어진 taskId를 가진 TaskMeasures 엔티티 중 완료시각이 null인 엔티티를 반환한다.
     TaskMeasures findByTaskIdAndCompleteTimeNull(Long taskId);
 
+    // 주어진 taskId를 가진 TaskMeasures 엔티티 중 완료시각이 null이 아닌 엔티티 목록을 반환한다.
     List<TaskMeasures> findByTaskIdAndCompleteTimeNotNull(Long taskId);
 
+    // 주어진 taskId를 가진 TaskMeasures 엔티티 목록을 반환한다.
     List<TaskMeasures> findByTaskId(Long taskId);
 }
