2e96768d933e2b8b527ad354c93f6c609157bf85
meohin <meohin.meo@gmail.com>
2023-12-08 18:38:56 +0900

    Chore(repository): TaskRepository 메서드 주석 업데이트
    TaskRepository 인터페이스의 메서드 주석을 추가하여 각 메서드가 어떤 동작을 하는지 표현했습니다.

--- src/main/java/com/meohin/todo_pe/repository/TaskRepository.java
+++ src/main/java/com/meohin/todo_pe/repository/TaskRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    // 특정 사용자에게 속한 모든 Task 엔티티 목록을 반환한다.
     List<Task> findTasksBySiteUser(SiteUser user);
+
+    // 주어진 subject를 포함하고 특정 사용자에게 속한 Task 엔티티 목록을 반환한다.
     List<Task> findTasksBySubjectContainsAndSiteUser(String subject, SiteUser user);
 }
