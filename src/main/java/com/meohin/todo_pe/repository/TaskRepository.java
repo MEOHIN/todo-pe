c6c671fe610f18b0d9953eccf1098e9828875047
meohin <meohin.meo@gmail.com>
2023-12-11 21:39:40 +0900

    Chore(repository): TaskRepository 인터페이스 정리
    - 사용하지 않는 findDistinctBySiteUserOrderByTaskMeasuresListStartTimeDesc 메서드를 제거했습니다.

--- src/main/java/com/meohin/todo_pe/repository/TaskRepository.java
+++ src/main/java/com/meohin/todo_pe/repository/TaskRepository.java
@@ -12,7 +12,4 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 
     // 주어진 subject를 포함하고 특정 사용자에게 속한 Task 엔티티 목록을 반환한다.
     List<Task> findTasksBySubjectContainsAndSiteUser(String subject, SiteUser user);
-
-    // 특정 사용자에게 속한 Task 엔티티를 시작시각을 기준으로 내림차순으로 정렬하여 중복을 제외하고 반환한다.
-    List<Task> findDistinctBySiteUserOrderByTaskMeasuresListStartTimeDesc(SiteUser user);
 }
