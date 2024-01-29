540a87309324dceab5dde6c7ace6c036504ca679
meohin <meohin.meo@gmail.com>
2024-01-29 17:02:29 +0900

    Feat: 주어진 subject를 포함한 Task 필터링 추가
    - 주어진 subject를 포함하고 특정 사용자에게 속한 Task 엔티티 목록을 반환하는 새로운 쿼리를 추가했습니다.

--- src/main/java/com/meohin/todo_pe/repository/TaskWithMeasuresRepository.java
+++ src/main/java/com/meohin/todo_pe/repository/TaskWithMeasuresRepository.java
@@ -36,4 +36,33 @@ public interface TaskWithMeasuresRepository extends JpaRepository<Task, Long> {
         ORDER BY tm.start_time DESC;
     """, nativeQuery = true)
     List<TaskWithMeasures> getTaskListWithMeasures(@Param("siteUserId") Long siteUserId);
+
+    // 주어진 subject를 포함하고 특정 사용자에게 속한 Task 엔티티 목록을 반환한다.
+    @Query(value = """
+        SELECT
+            t1.id as taskId,
+            t1.subject as subject,
+            t1.description as description,
+            t1.estimated_at as estimatedAt,
+            t1.status as status,
+            tm.id as measuresId,
+            tm.start_time as startTime,
+            tm.pause_time as pauseTime,
+            tm.continue_time as continueTime,
+            tm.elapsed_paused_time as elapsedTime
+        FROM (
+            SELECT t.*, MAX(tm.id) as tm_id
+            FROM task t
+            LEFT JOIN task_measures tm
+            on t.id = tm.task_id
+            WHERE t.site_user_id = :siteUserId
+            AND t.subject LIKE %:subject%
+            GROUP BY t.id
+        ) as t1
+        LEFT JOIN task_measures tm
+        on tm.id = tm_id
+        ORDER BY tm.start_time DESC;                                
+        """,
+            nativeQuery = true)
+    List<TaskWithMeasures> getTasksWithMeasuresBySubject(@Param("subject") String subject, @Param("siteUserId") Long siteUserId);
 }
