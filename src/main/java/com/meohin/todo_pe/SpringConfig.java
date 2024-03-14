0c7a1e46f025ea95969f48f6a2afac25ad4ea9f0
meohin <meohin.meo@gmail.com>
2024-03-14 10:31:18 +0900

    Feat: SpringConfig에 PasswordEncoder Bean 설정 추가
    SpringConfig 클래스에 PasswordEncoder를 반환하는 Bean 설정을 추가했습니다.

---
+++ src/main/java/com/meohin/todo_pe/SpringConfig.java
@@ -0,0 +1,18 @@
+package com.meohin.todo_pe;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Spring Security Context안에서 Scope가 제한된다.
+ */
+@Configuration
+public class SpringConfig {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
