5e42e694470bd090080251957e923e6b178d3a60
meohin <meohin.meo@gmail.com>
2024-03-14 10:31:58 +0900

    Chore: SecurityConfig에서 불필요한 Bean 설정 삭제
     SecurityConfig 클래스에서 AuthenticationManager를 반환하는 Bean 설정을 삭제했습니다.

--- src/main/java/com/meohin/todo_pe/SecurityConfig.java
+++ src/main/java/com/meohin/todo_pe/SecurityConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -24,10 +22,5 @@ public class SecurityConfig {
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                         .logoutSuccessUrl("/login").invalidateHttpSession(true));
         return httpSecurity.build();
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
