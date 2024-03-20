435413d60fc49aeca1f16b429be2ec3c4848a7b7
meohin <meohin.meo@gmail.com>
2024-03-20 19:35:46 +0900

    Refactor(controller): MainController에서 /timetodo 엔드포인트 제거 및 주석 추가
    - MainController에서 더 이상 사용되지 않는 /timetodo 엔드포인트를 제거했습니다.
    - 루트 엔드포인트(/)의 메서드에 대한 설명을 주석으로 추가했습니다.
    주석은 HTTP 프로토콜과 리다이렉션 동작에 대한 깊은 이해를 반영하며, 관련 동작을 더 잘 이해할 수 있도록 돕습니다.

--- src/main/java/com/meohin/todo_pe/controller/MainController.java
+++ src/main/java/com/meohin/todo_pe/controller/MainController.java
@@ -2,19 +2,19 @@ package com.meohin.todo_pe.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-
-    @RequestMapping("/timetodo")
-    @ResponseBody
-    public String index() {
-        return "안녕하세요 TimeTODO에 오신것을 환영합니다.";
-    }
-
     @RequestMapping("/")
     public String root() {
+        // viewResolver 에 의해서 요청한 클라이언트에게 회신.
+        // - (1) 보통은 template 하고 조립해서 http protocol에 의해서 html body를 담아서 회신.
+        // - (2) redirect가 붙으면 http protocol 구격에 해당하는 문자열을 회신.
+        // (1)는 body에 thymeleaf와 협력해서 만들어진 html 문자열을 body에 담아서 보냄. 
+        // (2)는 body 없음. http protocol 규격에 맞춤 문자열을 생성하는데, 헤더 정보를 담음.
+        //      상태코드느 304 이고, 이 때 담긴 추가 정보는 /task/list이고 브라우저가 이 데이터를 받아서 처리. 304니까 /task/list 로 자동으로 요청하게 됨.
+        // https://velog.io/@teddybearjung/HTTP-%EA%B5%AC%EC%A1%B0-%EB%B0%8F-%ED%95%B5%EC%8B%AC-%EC%9A%94%EC%86%8C
+        // 진짜 오피셜 정보는 RFC 문서다.
         return "redirect:/task/list";
     }
 }
